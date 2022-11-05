c3d437464c438b8dedda0e7a7831745c13fc845d
hwan-koo <korea9kh@naver.com>
2022-11-05 21:56:24 +0900

    Router(2)_arrange
    router folder made, import and export conception

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req,res) => res.send("Watch video");
+videoRouter.get("/watch",handleWatchVideo)
+
+export default videoRouter;
