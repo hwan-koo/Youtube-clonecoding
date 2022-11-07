2378b0ea2ba43d76ad44acc9907e764065bdfe41
hwan-koo <korea9kh@naver.com>
2022-11-07 17:13:57 +0900

    Pug setup
    pug start and base layout made

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const recommendedVideos = (req,res) => res.send("Homepage videos");
-export const see = (req,res) => res.send("Watch");
-export const edit = (req,res) => res.send("Edit");
+export const recommendedVideos = (req,res) => res.render("home",{pageTitle:"Home"});
+export const see = (req,res) => res.render("watch");
+export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search");
 export const deleteVideo = (req,res) => res.send("deleteVideo");
 export const upload = (req,res) => res.send("Upload");
