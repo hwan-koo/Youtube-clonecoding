be23a2ecf133933659b58ddf2ce430385e6b3929
hwan-koo <korea9kh@naver.com>
2022-11-09 00:55:52 +0900

    iterationm,mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,34 @@
-export const recommendedVideos = (req,res) => res.render("home",{pageTitle:"Home"});
+let videos = [
+    {
+        title : "First video",
+        rating: 4.8,
+        comments: 3,
+        createdAt: "3 minutes ago",
+        views: 77,
+        id: 1
+    },
+    {
+        title : "Second video",
+        rating: 4.8,
+        comments: 3,
+        createdAt: "3 minutes ago",
+        views: 77,
+        id: 2
+    },
+    {
+        title : "Third video",
+        rating: 4.8,
+        comments: 3,
+        createdAt: "3 minutes ago",
+        views: 77,
+        id: 3
+    }
+];
+
+export const recommendedVideos = (req,res) => {
+
+    return res.render("home",{pageTitle:"Home",videos });
+}
 export const see = (req,res) => res.render("watch");
 export const edit = (req,res) => res.render("edit");
 export const search = (req,res) => res.send("Search");
