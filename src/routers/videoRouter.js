3d4fb81ab42635e708e4dd374e7094b6d04caed8
hwan-koo <korea9kh@naver.com>
2022-11-16 19:04:30 +0900

    mongoDB
    CRUD concepts and start

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import {see, edit,deleteVideo,upload} from "../controllers/videoController";
+import {watch, getEdit,postEdit,getUpload,postUpload,deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 
-videoRouter.get("/upload",upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete",deleteVideo);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+
 
 export default videoRouter;
