2fee88664e3464b428ae10b9267939ba1c8f83c9
hwan-koo <korea9kh@naver.com>
2022-12-04 23:56:18 +0900

    Update init.js

--- src/init.js
+++ src/init.js
@@ -7,7 +7,7 @@ import "./models/User";
 import app from "./server";
  
 // start our application
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
